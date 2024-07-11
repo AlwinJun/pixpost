94f63677269a59e2b3475ba72153326fb314ca11
AlwinJun <mahidlawonalwin@gmail.com>
2024-07-11 21:53:25 +0800

    Add featured job section and jobcard, section heading components

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,23 @@
+<article class="flex flex-col rounded-xl bg-white/10 p-4 text-center">
+    <div class="text-left">GovExec</div>
+
+    <div class="py-8 font-bold">
+        <h4>
+            Full stack Laravel Developer
+        </h4>
+        <p>Full Time - From $60,000</p>
+    </div>
+
+    <div class="mt-auto flex items-center justify-between">
+        <div>
+            <a href="#"
+                class="hover:bg-white/35 rounded-xl bg-white/20 px-2 py-1 text-xs transition-colors duration-200">Frontend</a>
+            <a href="#"
+                class="hover:bg-white/35 rounded-xl bg-white/20 px-2 py-1 text-xs transition-colors duration-200">Backend</a>
+            <a href="#"
+                class="hover:bg-white/35 rounded-xl bg-white/20 px-2 py-1 text-xs transition-colors duration-200">API</a>
+        </div>
+
+        <img src="https://placehold.co/42x42" alt="logo" class="rounded-xl">
+    </div>
+</article>
---
+++ resources/views/components/section-heading.blade.php
@@ -0,0 +1,4 @@
+<div class="flex items-center gap-x-2">
+    <span class="size-2 bg-white"></span>
+    <h3 class="text-lg font-bold">{{ $slot }}</h3>
+</div>
