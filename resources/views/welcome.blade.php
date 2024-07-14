8843df2bb0b04a2d354d845ea5c11abbe7f0f1ce
AlwinJun <mahidlawonalwin@gmail.com>
2024-07-14 21:41:49 +0800

    Add search bar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,14 @@
 <x-layout>
     <div class="space-y-10">
-        <section>
+        <section class="pt-6 text-center">
+            <h1 class="text-4xl font-bold">Let's Find Your Next Job</h1>
+            <form action="" class="mt-6">
+                <input type="search" name="" placeholder="Web Developer.."
+                    class="w-full max-w-lg rounded-xl border border-white/10 bg-white/5 px-4 py-3" />
+            </form>
+        </section>
+
+        <section class="pt-10">
             <x-section-heading>Featured Jobs</x-section-heading>
             <div class="mt-6 grid gap-4 lg:grid-cols-3">
                 <x-job-card />
