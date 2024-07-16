e388f06eee66ecfb424f038f2d81f51812d1c7c6
AlwinJun <mahidlawonalwin@gmail.com>
2024-07-16 14:28:43 +0800

    Add fake fields

--- database/factories/JobFactory.php
+++ database/factories/JobFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Employer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,13 @@ class JobFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'employer_id' => Employer::factory(),
+            'title' => fake()->jobTitle(),
+            'salary' => fake()->randomElement(['20,000 PHP', '25,000 PHP', '45,000 PHP', '50,000 PHP','75,000 PHP', '100,000 PHP']),
+            'location' => fake()->country(),
+            'schedule' => fake()->randomElement(['Full Time', 'Part Time']),
+            'url' => fake()->url(),
+            'featured' => fake()->boolean(),
         ];
     }
 }
